89bb3e1c8aea27f1114cf003c05699ea3fd562ee
Alexander Ioannidis <a.ioannidis@cern.ch>
2019-06-16 05:58:28 +0200

    add "-o" flag to server.py to open in browser

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from livereload import Server, shell
 import subprocess
+import sys
 
 # run initial build
 subprocess.run('./build.py')
@@ -10,4 +11,6 @@ server.watch('base.jinja2', './build.py')
 server.watch('content/*.md', './build.py')
 server.watch('content/blog/*.md', './build.py')
 server.watch('*.css', './build.py')
-server.serve(root='_build/')
+
+open_in_browser = '-o' in sys.argv
+server.serve(root='_build/', open_url=open_in_browser)
